e8ec61267b211c5594bb3acd3dbb5a535097ed9f
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2015-05-23 17:36:47 +0200

    add validations to Room names

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires a name' do
+    expect(Room.new).to be_invalid
+  end
+  it 'does not allow multiple records to have the same name' do
+    Room.create!(name: 'foo')
+    expect(Room.new(name: 'foo')).to be_invalid
+  end
 end

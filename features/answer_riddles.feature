a6d62ece1ae7ef8ffa0456656de9a02d720b0c7a
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2015-05-24 11:49:56 +0200

    clarify feature: Answer riddles

--- features/answer_riddles.feature
+++ features/answer_riddles.feature
@@ -3,9 +3,15 @@ Feature: Answer riddles
   As a user
   I want to answer some riddles
 
-  Scenario: Select correct answer
+  Scenario: Change selection
     Given I see a riddle
-    When I select the correct answer
-    And I wait for the round to close
-    Then I should see a message Correct answer
+    When I select the first answer
+    Then the first answer should be shown as selected
+    When I select the second answer
+    Then the second answer should be shown as selected
+
+  Scenario: Feedback on correct answer
+    Given I selected the correct answer
+    When the riddle closes
+    Then I should see a message Your answer is correct
 
--- features/step_definitions/answer_riddles_steps.rb
+++ features/step_definitions/answer_riddles_steps.rb
@@ -2,6 +2,14 @@ require 'riddle/riddle_master'
 
 EXAMPLE_ROOM_NAME = 'ExampleRoomName'
 
+def riddle_master
+  RiddleMaster.find_or_create(EXAMPLE_ROOM_NAME)
+end
+
+def riddle
+  riddle_master.current_riddle
+end
+
 Before do
   Room.delete_all
 end
@@ -17,11 +25,24 @@ Given(/^I see a riddle$/) do
   click_button 'Enter'
 end
 
-When(/^I select the correct answer$/) do
-  riddle = RiddleMaster.find_or_create(EXAMPLE_ROOM_NAME).current_riddle
-  click_button riddle.correct_answer
+When(/^I select the (.+) answer$/) do |position|
+  @selected_answer = riddle.answers.send(postition.to_sym)
+  click_button @selected_answer
 end
 
-When(/^I wait for the round to close$/) do
+Then(/^the (.+) answer should be shown as selected$/) do |postition|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the correct answer is revealed$/) do
+  pending
+  # expect(page).to have_content('The correct answer is')
+end
+
+Given(/^I selected the correct answer$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the riddle closes$/) do
   pending # express the regexp above with the code you wish you had
 end
